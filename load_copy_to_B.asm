fb2ff3b89ea55c05d2740cf2a6b71d46fceada38
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2022-02-06 21:03:54 +0530

    Update load_copy_to_B.asm

--- load_copy_to_B.asm
+++ load_copy_to_B.asm
@@ -1,6 +1,10 @@
 ; Mayank Dutta
 ; 19BEE050
 
+; Load the content of the memory location 2500H directly to the
+; accumulator, then transfer it to register B. 
+; the content of memory location 2500H is OAH.
+
 LDA 2500H
 MOV B, A
 HLT
